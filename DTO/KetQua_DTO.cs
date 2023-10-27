8f3afafb5a4488c85ea0ee4ef0b01354725de7a6
lethai02 <lethai03102002@gmail.com>
2023-10-27 15:48:42 +0700

    thai

--- DTO/KetQua_DTO.cs
+++ DTO/KetQua_DTO.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace DTO
 {
-    internal class KetQua_DTO
+    public class KetQua_DTO
     {
+        private int maSo;
+        private string maMH;
+        private int Diem;
+        public KetQua_DTO() { }
+
+        public KetQua_DTO(int maSo, string maMH, int Diem)
+        {
+            this.maSo = maSo;
+            this.maMH = maMH;
+            this.Diem = Diem;
+        }
+
+        public int MaSo { get => maSo; set => maSo = value; }
+        public string MaMH { get => maMH; set => maMH = value; }
+        public int DiemSo  { get => Diem; set => Diem = value; }
     }
 }
