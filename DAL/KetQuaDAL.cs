2fff88382261f9c04d9833ed8c065e27e3bb8fe4
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-10-27 21:39:10 +0700

    update

---
+++ DAL/KetQuaDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+namespace DAL
+{
+    public class KetQuaDAL : MSSQLConnect
+    {
+        public bool insertKetQua(KetQua_DTO kq)
+        {
+            try
+            {
+                Connect();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into ketqua values (@MaSo, @MaMH, @Diem)";
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@MaSo", kq.MaSo).SqlDbType = SqlDbType.Int;
+                cmd.Parameters.AddWithValue("@MaMH", kq.MaMH).SqlDbType = SqlDbType.NVarChar;
+                cmd.Parameters.AddWithValue("@Diem", kq.DiemSo).SqlDbType = SqlDbType.Int;
+               
+
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi:" + ex.Message);
+                return false;
+            }
+            finally
+            {
+                Disconnect();
+            }
+
+        }
+    }
+}
--- GUI/GiaoDienGUI.Designer.cs
+++ GUI/GiaoDienGUI.Designer.cs
@@ -28,160 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GiaoDienGUI));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chứcNăngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hỗTrợToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.btnOpenSinhVien = new System.Windows.Forms.ToolStripButton();
             this.btnKhoa = new System.Windows.Forms.ToolStripButton();
             this.btnMonHoc = new System.Windows.Forms.ToolStripButton();
             this.btnNhapDiem = new System.Windows.Forms.ToolStripButton();
             this.btnXemDiem = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.btnThongKeKhoa = new System.Windows.Forms.ToolStripButton();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.hệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.chứcNăngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.hỗTrợToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pnMain = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
-            this.flowLayoutPanel2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.pnMain);
-            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(984, 561);
+            this.panel1.Size = new System.Drawing.Size(1140, 700);
             this.panel1.TabIndex = 0;
             // 
-            // flowLayoutPanel1
+            // pnMain
             // 
-            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(984, 101);
-            this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.AutoSize = false;
-            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnOpenSinhVien,
-            this.btnKhoa,
-            this.btnMonHoc,
-            this.btnNhapDiem,
-            this.btnXemDiem,
-            this.toolStripButton1,
-            this.btnThongKeKhoa});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(474, 40);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "Sinh viên";
-            // 
-            // btnOpenSinhVien
-            // 
-            this.btnOpenSinhVien.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnOpenSinhVien.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenSinhVien.Image")));
-            this.btnOpenSinhVien.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnOpenSinhVien.Name = "btnOpenSinhVien";
-            this.btnOpenSinhVien.Size = new System.Drawing.Size(59, 37);
-            this.btnOpenSinhVien.Text = "Sinh viên";
-            // 
-            // btnKhoa
-            // 
-            this.btnKhoa.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnKhoa.Image = ((System.Drawing.Image)(resources.GetObject("btnKhoa.Image")));
-            this.btnKhoa.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnKhoa.Name = "btnKhoa";
-            this.btnKhoa.Size = new System.Drawing.Size(38, 37);
-            this.btnKhoa.Text = "Khoa";
-            this.btnKhoa.Click += new System.EventHandler(this.btnKhoa_Click);
-            // 
-            // btnMonHoc
-            // 
-            this.btnMonHoc.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnMonHoc.Image = ((System.Drawing.Image)(resources.GetObject("btnMonHoc.Image")));
-            this.btnMonHoc.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnMonHoc.Name = "btnMonHoc";
-            this.btnMonHoc.Size = new System.Drawing.Size(59, 37);
-            this.btnMonHoc.Text = "Môn học";
-            // 
-            // btnNhapDiem
-            // 
-            this.btnNhapDiem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnNhapDiem.Image = ((System.Drawing.Image)(resources.GetObject("btnNhapDiem.Image")));
-            this.btnNhapDiem.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnNhapDiem.Name = "btnNhapDiem";
-            this.btnNhapDiem.Size = new System.Drawing.Size(70, 37);
-            this.btnNhapDiem.Text = "Nhập điểm";
-            // 
-            // btnXemDiem
-            // 
-            this.btnXemDiem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnXemDiem.Image = ((System.Drawing.Image)(resources.GetObject("btnXemDiem.Image")));
-            this.btnXemDiem.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnXemDiem.Name = "btnXemDiem";
-            this.btnXemDiem.Size = new System.Drawing.Size(65, 37);
-            this.btnXemDiem.Text = "Xem điểm";
-            // 
-            // toolStripButton1
-            // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(89, 37);
-            this.toolStripButton1.Text = "Thống kê khoa";
-            // 
-            // btnThongKeKhoa
-            // 
-            this.btnThongKeKhoa.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
-            this.btnThongKeKhoa.Image = ((System.Drawing.Image)(resources.GetObject("btnThongKeKhoa.Image")));
-            this.btnThongKeKhoa.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnThongKeKhoa.Name = "btnThongKeKhoa";
-            this.btnThongKeKhoa.Size = new System.Drawing.Size(41, 37);
-            this.btnThongKeKhoa.Text = "Thoát";
+            this.pnMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(236)))), ((int)(((byte)(247)))));
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 66);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(1140, 634);
+            this.pnMain.TabIndex = 1;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.AutoSize = false;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.hệThốngToolStripMenuItem,
             this.chứcNăngToolStripMenuItem,
             this.hỗTrợToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(207, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1140, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-            // 
-            // flowLayoutPanel2
-            // 
-            this.flowLayoutPanel2.Controls.Add(this.menuStrip1);
-            this.flowLayoutPanel2.Controls.Add(this.toolStrip1);
-            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(474, 81);
-            this.flowLayoutPanel2.TabIndex = 3;
             // 
             // hệThốngToolStripMenuItem
             // 
@@ -201,32 +102,119 @@
             this.hỗTrợToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
             this.hỗTrợToolStripMenuItem.Text = "Hỗ trợ";
             // 
-            // pnMain
+            // toolStrip1
             // 
-            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnMain.Location = new System.Drawing.Point(0, 101);
-            this.pnMain.Name = "pnMain";
-            this.pnMain.Size = new System.Drawing.Size(984, 460);
-            this.pnMain.TabIndex = 1;
+            this.toolStrip1.AutoSize = false;
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(175)))), ((int)(((byte)(248)))));
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenSinhVien,
+            this.btnKhoa,
+            this.btnMonHoc,
+            this.btnNhapDiem,
+            this.btnXemDiem,
+            this.btnThongKeKhoa});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1140, 40);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "Sinh viên";
+            // 
+            // btnOpenSinhVien
+            // 
+            this.btnOpenSinhVien.AutoSize = false;
+            this.btnOpenSinhVien.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenSinhVien.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenSinhVien.Image")));
+            this.btnOpenSinhVien.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpenSinhVien.Name = "btnOpenSinhVien";
+            this.btnOpenSinhVien.Size = new System.Drawing.Size(100, 37);
+            this.btnOpenSinhVien.Text = "Sinh viên";
+            this.btnOpenSinhVien.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnKhoa
+            // 
+            this.btnKhoa.AutoSize = false;
+            this.btnKhoa.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhoa.Image = ((System.Drawing.Image)(resources.GetObject("btnKhoa.Image")));
+            this.btnKhoa.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnKhoa.Name = "btnKhoa";
+            this.btnKhoa.Size = new System.Drawing.Size(100, 37);
+            this.btnKhoa.Text = "Khoa";
+            this.btnKhoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnKhoa.Click += new System.EventHandler(this.btnKhoa_Click);
+            // 
+            // btnMonHoc
+            // 
+            this.btnMonHoc.AutoSize = false;
+            this.btnMonHoc.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMonHoc.Image = ((System.Drawing.Image)(resources.GetObject("btnMonHoc.Image")));
+            this.btnMonHoc.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnMonHoc.Name = "btnMonHoc";
+            this.btnMonHoc.Size = new System.Drawing.Size(100, 37);
+            this.btnMonHoc.Text = "Môn học";
+            this.btnMonHoc.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnNhapDiem
+            // 
+            this.btnNhapDiem.AutoSize = false;
+            this.btnNhapDiem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhapDiem.Image = ((System.Drawing.Image)(resources.GetObject("btnNhapDiem.Image")));
+            this.btnNhapDiem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNhapDiem.Name = "btnNhapDiem";
+            this.btnNhapDiem.Size = new System.Drawing.Size(110, 37);
+            this.btnNhapDiem.Text = "Nhập điểm";
+            this.btnNhapDiem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnNhapDiem.Click += new System.EventHandler(this.btnNhapDiem_Click);
+            // 
+            // btnXemDiem
+            // 
+            this.btnXemDiem.AutoSize = false;
+            this.btnXemDiem.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXemDiem.Image = ((System.Drawing.Image)(resources.GetObject("btnXemDiem.Image")));
+            this.btnXemDiem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnXemDiem.Name = "btnXemDiem";
+            this.btnXemDiem.Size = new System.Drawing.Size(110, 37);
+            this.btnXemDiem.Text = "Xem điểm";
+            this.btnXemDiem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnThongKeKhoa
+            // 
+            this.btnThongKeKhoa.AutoSize = false;
+            this.btnThongKeKhoa.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThongKeKhoa.Image = ((System.Drawing.Image)(resources.GetObject("btnThongKeKhoa.Image")));
+            this.btnThongKeKhoa.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnThongKeKhoa.Name = "btnThongKeKhoa";
+            this.btnThongKeKhoa.Size = new System.Drawing.Size(110, 37);
+            this.btnThongKeKhoa.Text = "Thống kê";
+            this.btnThongKeKhoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.toolStrip1);
+            this.panel2.Controls.Add(this.menuStrip1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1140, 66);
+            this.panel2.TabIndex = 3;
             // 
             // GiaoDienGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ClientSize = new System.Drawing.Size(1140, 700);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "GiaoDienGUI";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý sinh viên";
             this.panel1.ResumeLayout(false);
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.flowLayoutPanel2.ResumeLayout(false);
-            this.flowLayoutPanel2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -234,21 +222,18 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton btnOpenSinhVien;
         private System.Windows.Forms.ToolStripButton btnXemDiem;
         private System.Windows.Forms.ToolStripButton btnMonHoc;
         private System.Windows.Forms.ToolStripButton btnNhapDiem;
         private System.Windows.Forms.ToolStripButton btnKhoa;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
         private System.Windows.Forms.ToolStripButton btnThongKeKhoa;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem hệThốngToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem chứcNăngToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hỗTrợToolStripMenuItem;
         private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Panel panel2;
     }
 }
